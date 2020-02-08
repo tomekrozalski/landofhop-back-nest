0f4de48fa3d940f79e6ac92211b38714e357767b
Tomek Rozalski <alium@daucusetanser.pl>
2020-02-08 23:13:52 +0100

    [Beverage] Add AuthGuard. Start playing with tracedSvg using potrace

--- src/beverage/beverage.controller.ts
+++ src/beverage/beverage.controller.ts
@@ -1,12 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 
 import { SiteLanguage } from 'utils/enums';
+import { AuthGuard } from 'utils/guards';
 import {
 	NormalizedBeverage,
 	NormalizedTranslatedBeverage,
 	TranslatedBeverageBasics,
 } from 'utils/types/normalized';
 import { BeverageService } from './beverage.service';
+import * as potrace from 'potrace';
+
+
+
 
 @Controller('beverage')
 export class BeverageController {
@@ -23,6 +28,32 @@ export class BeverageController {
 	async getAllBeverages() {
 		const beverages: NormalizedBeverage[] = await this.beverageService.getAllBeverages();
 		return beverages;
+	}
+
+	@Get('create-traced-svgs/:shortId/:brand/:badge')
+	// @UseGuards(AuthGuard)
+	async getTracedSVGs(
+		@Param('shortId') shortId: string,
+		@Param('brand') brand: string,
+		@Param('badge') badge: string,
+	) {
+
+		await potrace.trace('https://land-of-hop-images.s3.eu-central-1.amazonaws.com/browar-stu-mostow/baltic-porter/93r65i/cover/jpg/4x.jpg', {
+			color: '#666',
+			threshold: 200,
+			turdSize: 20,
+			alphaMax: 20,
+		}, (err: any, svg: string) => {
+
+			if (err) {
+				return false;
+			}
+
+			console.log('svg', svg);
+
+			return svg;
+		});
+
 	}
 
 	@Get(':language/:shortId/:brand/:badge')
@@ -52,6 +83,8 @@ export class BeverageController {
 
 		return beverage;
 	}
+
+
 
 	// @Put(':badge')
 	// updateBeverage(@Param('badge') badge: string, @Body('added') added: string) {
