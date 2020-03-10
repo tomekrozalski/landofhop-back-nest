3c2c88556222b9135cac0ea31c8eb56a08159a60
Tomek Rozalski <alium@daucusetanser.pl>
2020-03-10 22:54:18 +0100

    [Beverage] Server does not work. Try to fix

--- src/utils/s3-interactions/beverage/saveCover.ts
+++ src/utils/s3-interactions/beverage/saveCover.ts
@@ -3,6 +3,11 @@ import * as sharp from 'sharp';
 
 const s3 = new aws.S3({});
 
+aws.config.update({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
 export enum ImageSize {
 	large,
 	big,
