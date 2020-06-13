d42d3f2ccd43d05f19582fd36bcc197822e40984
Tomek Rozalski <alium@daucusetanser.pl>
2020-06-13 18:23:30 +0200

    [Beverage] Keep work on language mapping. I stacked now on cooperation array. We have here array inside array. I will come back to it tomorrow

--- src/utils/schemas/beverage/statics/common/aggregation/beverage/language.ts
+++ src/utils/schemas/beverage/statics/common/aggregation/beverage/language.ts
@@ -1,4 +1,6 @@
 const language = [
+  // ------------------------------------------------
+  // Label - name, series, brand, cooperation
   {
     $set: {
       'label.general.name_info': {
@@ -29,6 +31,126 @@ const language = [
       'label.general.name': {
         $map: {
           input: '$label.general.name_info',
+          as: 'names',
+          in: {
+            language: '$$names.language.code',
+            value: '$$names.value',
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      'label.general.series_info': {
+        $map: {
+          input: '$label.general.series',
+          as: 'names',
+          in: {
+            language: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$language',
+                    as: 'values',
+                    cond: { $eq: ['$$values.id', '$$names.language'] },
+                  },
+                },
+                0,
+              ],
+            },
+            value: '$$names.value',
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      'label.general.series': {
+        $map: {
+          input: '$label.general.series_info',
+          as: 'names',
+          in: {
+            language: '$$names.language.code',
+            value: '$$names.value',
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      'label.general.brand_language_info': {
+        $map: {
+          input: '$label.general.brand_info.name',
+          as: 'names',
+          in: {
+            language: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$language',
+                    as: 'values',
+                    cond: { $eq: ['$$values.id', '$$names.language'] },
+                  },
+                },
+                0,
+              ],
+            },
+            value: '$$names.value',
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      'label.general.brand_info.name': {
+        $map: {
+          input: '$label.general.brand_language_info',
+          as: 'names',
+          in: {
+            language: '$$names.language.code',
+            value: '$$names.value',
+          },
+        },
+      },
+    },
+  },
+
+  // ------------------------------------------------
+  // Producer - series
+  {
+    $set: {
+      'producer.general.series_info': {
+        $map: {
+          input: '$producer.general.series',
+          as: 'names',
+          in: {
+            language: {
+              $arrayElemAt: [
+                {
+                  $filter: {
+                    input: '$language',
+                    as: 'values',
+                    cond: { $eq: ['$$values.id', '$$names.language'] },
+                  },
+                },
+                0,
+              ],
+            },
+            value: '$$names.value',
+          },
+        },
+      },
+    },
+  },
+  {
+    $set: {
+      'producer.general.series': {
+        $map: {
+          input: '$producer.general.series_info',
           as: 'names',
           in: {
             language: '$$names.language.code',
