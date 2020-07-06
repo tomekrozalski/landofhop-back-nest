26313fdb8a1a2d961d440a44e10bfce2bb55bb94
Tomek Rozalski <alium@daucusetanser.pl>
2020-07-06 12:28:13 +0200

    [Temp] Add 15 beverages to temporary database

--- temp/beverages.js
+++ temp/beverages.js
@@ -177,4 +177,394 @@ const beverages = [
     price: '6.00 zł',
     added: '27 czerwca 2020, 20:27',
   },
+  // zakupy
+  {
+    brand: 'Browar Nepomucen',
+    cooperation: 'Funky Fluid',
+    extract: '16.5 stopni BLG',
+    alkochol: '7%',
+    name: "Let's Get Hazy, no 1",
+    style: 'DDH NEIPA',
+    tale: {
+      pl:
+        "Let's get crazy<skreślone> hazy to propozycja piwa na ciepłe miesiące, którą przygotowaliśmy we współpracy z browarem Funky Fluid. Oddaj się przygodzie pełnej ekstremalnych aromatów chmielu i cytrusowej soczystości.",
+      en:
+        "Let's get crazy<skreślone> hazy is a beer offer for warm months, which we have prepared in cooperation with the Funky Fluid brewery.\nTake on an adventure full of extreme hop aromas and citrus juiciness.",
+    },
+    barcode: '5907709756397',
+    place: 'Jutrosin',
+    container: 'puszka czarna 500ml',
+    hoprate: '20g/l',
+    bitterness: '2/6',
+    ingredients: {
+      pl:
+        'woda, słód jęczmienny, słód pszeniczny, płatki owsiane, płatki pszenne, słód owsiany, płatki ryżowe, chmiel: Columbus, Centennial, Simcoe, Amarillo, Citra, drożdże: Hazy Daze (z własnej stacji propagacji)',
+      en:
+        'barley malt, wheat malt, oat flakes, wheat flakes, oat malt, rice flakes, hops: Columbus, Centennial, Simcoe, Amarillo, Citra, Hazy Daze yeast (from own propagation)',
+    },
+    temperature: '10-12',
+    filtration: false,
+    pasteurisation: true,
+    height: 17,
+    price: 12.5,
+    added: '05.07.2020, 21:08',
+  },
+  {
+    brand: 'Pinta',
+    series: 'Pinta Deli Store #3',
+    name: 'Pastry Sour. Cherry, Raspberry, Blackberry, Black Currant',
+    barcode: '5908252864553',
+    container: 'puszka czarna 500ml',
+    height: 17,
+    extract: '20 stopni BLG',
+    alkochol: '5.6% obj.',
+    tale: {
+      pl:
+        'W Pinta Deli Store #3 stawiamy na mocno owocowy skład. Ponadprzeciętną gładkość zapewnia dodatek laktozy i kremowa baza słodowa. Czeka na Ciebie prawdziwy, słodko-kwaśny koktajl!',
+      en:
+        'At Pinta Deli Store #3 we focus on a very fruity composition. Unbelievable smoothness is delivered by lactose and a creamy malt base. A real sweet and sour cocktail is waiting for you!',
+    },
+    pasteurisation: true,
+    filtration: false,
+    ingredients:
+      'woda, słód jęczmienny pilzneński, płatki owsiane błyskawiczne, płatki pszenne błyskawiczne, laktoza, chmiel: Columbus (USA), sok z wiśni, sok z maliny, sok z jeżyny, sok z czarnej porzeczki, drożdże WLP066 London Fog',
+    place: 'Wieprz',
+    price: 11.3,
+    added: '05.07.2020, 21:16',
+  },
+  {
+    brand: 'Pinta',
+    series: 'Pinta Deli Store #2',
+    name: 'Pastry Sour. Peach, Apricot, Nectarine, Passion Fruit',
+    barcode: '5908252864522',
+    container: 'puszka czarna 500ml',
+    height: 17,
+    extract: '20 stopni BLG',
+    alkochol: '5.6% obj.',
+    tale: {
+      pl:
+        'Pinta Deli Store #2 to soczystość i wyjątkowa gładkość. Smak prawdziwych owoców i odrobina laktozy na kremowej bazie słodowej. Kompozycja, która skutecznie przekonuje do stylu Pastry Sour!',
+
+      en:
+        'Pinta Deli Store #2 stands for juiciness and exceptional smoothness. The taste of real fruits and a bit of lactose on a creamy malt base. A composition that strongly convinces to the Pastry Sour style!',
+    },
+    pasteurisation: true,
+    filtration: false,
+    ingredients:
+      'woda, słód jęczmienny pilzneński, płatki owsiane błyskawiczne, płatki pszenne błyskawiczne, laktoza, chmiel: Columbus (USA), puree z brzoskwini, puree z moreli, puree z nektarynki, puree z marakui, drożdże WLP066 London Fog',
+    place: 'Wieprz',
+    price: 11.3,
+    added: '05.07.2020, 21:20',
+  },
+  {
+    brand: 'Browar Profesja',
+    name: 'Kanclerz',
+    style: 'Blackthorn Berliner Weisse (z tarniną)',
+    alkochol: '3,6% obj.',
+    extract: '9 stopni Plato',
+    temperature: '8-12',
+    ingredients:
+      'woda, słód jęczmienny, słód pszeniczny, zagęsczony sok z tarniny, chmiele, drożdże Brettanomyces Bruxellensis Trius, bakterie Lactobacillus Brevis, Lactococcus Lactis, Lactobacillus Plantarum, Lactobacillus Delbrueckii',
+    filtration: false,
+    pasteurisation: true,
+    bitterness: '0.5/5',
+    place: 'Wrocław',
+    barcode: '5904730658355',
+    container: 'butelka brązowa 500ml',
+    height: 25,
+    price: 9.5,
+    added: '05.07.2020, 22:06',
+    tale: `
+Smak: tarninowy, owocowy, kwaśny
+Aromat: tarninowy, śliwkowy, kwaśny
+Barwa: ciemnośliwkowa
+
+Używamy wyłącznie płynnych drożdży własnej propagacji
+Może zawierać naturalny osad drożdżowy
+    `,
+  },
+  {
+    brand: 'Trzech Kumpli',
+    name: 'Gose',
+    series: ['sour brews', 'kwaśne warki'],
+    style: ['Gose, Mango & Passion Fruit', 'Gose, z mango i marakują'],
+    ingredients: {
+      pl:
+        'woda, słód jęczmienny, płatki pszenne, słód pszeniczny, puree z mango, puree z marakui, chmiel, drożdże, bakterie kwasu mlekowego lactobacillus',
+    },
+    alcohol: '4.9% ABV',
+    extract: '12.5% Plato',
+    container: 'butelka 500ml brązowa',
+    contract: 'Browar Zapanbrat',
+    place: 'Żywiec',
+    barcode: '5905669479752',
+    tale: `
+Gose to lekkie, pszeniczny, słono-kwaśne piwo, które narodziło się w Dolnej Saksonii i prawie wyginęło jako styl piwny na początku XX w. Dopiero u zarania dwudziestego pierwszego stulecia staraniem małych niemieckich browarów rzemieślniczych zaczęło wracać do żywych.
+
+Historycznie Gose było piwem fermentacji spontanicznej, a więc przeprowadzanej przez dzikie drożdże. Legenda głosi, że swój słony smak zawdzięczało charakterycznemu profilowi wody rzeki Gose przepływającej przez miasto Goslar.
+
+My postanowiliśmy pójść krok dalej i klasyczne Gose połączyliśmy z przecierem z mango oraz marakui. Otrzymaliśmy piwo wyjątkowo rześkie, o idealnym balansie między smakami słonym, kwaśnym, gorzkim i słodkim.
+To pierwsze piwo kwaśne w naszym portfolio i doskonały start przygody z piwami kwaśnymi.
+
+Nie wahaj się!
+    `,
+    fermentation: 'top',
+    filtration: false,
+    pasteurisation: true,
+    height: 25,
+    price: 10.2,
+    added: '05.07.2020, 22:17',
+  },
+  {
+    name: 'Mam co robić',
+    brand: 'Browar zakładowy',
+    style: 'Hazy Double IPA, Mosaic Ekuanot Rakau',
+    filtration: false,
+    pasteurisation: false,
+    extract: '18% wag.',
+    alcohol: '7.7% obj.',
+    ingredients: 'woda, słód jęczmienny, płatki ryżowe, chmiel, drożdże',
+    temperature: '6-8',
+    place: 'Poniatowa',
+    contrainer: 'butelka 500ml brązowa',
+    barcode: '5907753170385',
+    height: 25,
+    price: 12.1,
+    added: '05.07.2020, 22:22',
+  },
+  {
+    name: 'Na pewno!',
+    brand: 'Browar zakładowy',
+    style: 'New England IPA, Citra Sabro',
+    filtration: false,
+    pasteurisation: false,
+    extract: '16% wag.',
+    alcohol: '6% obj.',
+    ingredients:
+      'woda, słód jęczmienny, płatki pszenne, płatki owsiane, chmiel, drożdże',
+    temperature: '6-8',
+    place: 'Poniatowa',
+    contrainer: 'butelka 500ml brązowa',
+    barcode: '5907753170392',
+    height: 25,
+    price: 11,
+    added: '05.07.2020, 22:24',
+  },
+  {
+    name: 'Cyrulik',
+    brand: 'Browar Profesja',
+    style: 'Smoked Berliner Weisse',
+    alcohol: '3.6% obj.',
+    extract: '9 stopni Plato',
+    temperature: '8-12',
+    tale: `
+Smak: kwaśny, wędzony, owocowy
+Aromat: wędzony, owocowy, kwaśny
+Barwa: jasnosłomkowa
+
+Używamy wyłącznie płynnych drożdży własnej propagacji
+Może zawierać naturalny osad drożdżowy
+    `,
+    ingredients:
+      'woda, słód jęczmienny wędzony bukiem, słód pszeniczny, chmiele, drożdże Grodziskie, bakterie Lactobacillus Brevis, Lactococcus Lactis, Lactobacillus Plantarum, Lactobacillus Delbrueckii',
+    filtration: false,
+    pasteurisation: true,
+    bitterness: '0.5/5',
+    place: 'Wrocław',
+    barcode: '5904730658133',
+    container: 'butelka brązowa 500ml',
+    height: 25,
+    price: 8.5,
+    added: '05.07.2020, 22:31',
+  },
+  {
+    name: 'We are not alone here!',
+    style: 'Sweet & Sour Smoothie IPA',
+    brand: 'Browar Nepomucen',
+    temperature: '10-12',
+    filtration: false,
+    pasteurisation: true,
+    bitterness: '1/6',
+    extract: '18 stopni',
+    alcohol: '7%',
+    container: 'butelka 500ml brązowa',
+    barcode: '5907709756281',
+    ingredients:
+      'woda, słód jęczmienny, słód pszeniczny, ananas, marakuja, płatki pszenne, płatki owsiane, laktoza, maltodekstryna, chmiele: odmiany amerykański, drożdże: górnej fermentacji',
+    place: 'Jutrosin',
+    height: 25,
+    price: 11.2,
+    added: '05.07.2020, 22:35',
+  },
+  {
+    brand: 'AleBrowar',
+    series: '#AleLab',
+    name: '01A Fallen Down Horse',
+    style: 'Hoppy Witbier',
+    ingredients:
+      'woda, słody: pilzneński (56,25%), pszeniczny (15,63%), Oat Malt (9,38%), płatki owsiane (11,25%), płatki pszenne (7,50%), chmiel: Hallertau Blanc, Chinook, Citra, Ekuanot, Citra, drożdże: London Fog',
+    tale: `
+Założenia. Opis Smaku.
+
+Tradycyjne pszeniczne piwo belgijskie z dodatkiem kolendry i skórki gorzkiej pomarańczy z wyraźnym twistem w kierunku mocno chmielonych nowofalowych piw w stylu American IPA. To połączenie nadaje temu piwu mocny chmielowo cytrusowy charakter. Idealnie odświeżający w gorące dni.
+
+Notatki
+
+Pierwsze piwo w serii #AleLab, w której zapraszamy Ciebie do rozwijania naszych receptur. Oznacz swoją opinię, sugestię na Instagramie hashtagiem #AleLAB01A - a my weźmiemy ją pod uwagę przy kolejnej warce tego piwa.
+
+EleEkipa
+
+Zacieranie:
+65 stopni C - 75 min
+78 stopni C - 1 min
+Gotowanie: 30 minut
+    `,
+    barcode: '5907771340654',
+    container: 'puszka 500ml srebrna',
+    alcohol: '5,3 obj.',
+    extract: '14% wag.',
+    filtration: false,
+    pasteurisation: false,
+    place: 'Lębork',
+    price: 11.8,
+    added: '06.07.2020, 11:17',
+  },
+  {
+    brand: 'AleBrowar',
+    series: '#AleLab',
+    name: "02A It's not another cola",
+    style: 'Nitro Cola Stout',
+    ingredients:
+      'woda, słody: Pale Ale (64,71%), Carapils (2.94%), pszeniczny (5.88%), wiedeński (5.88%), Carafa Special typ 3 (2.94%), Carafa Special typ 2 (2.94%), Simpsons Chocolate (2.94%), Laktoza (11,76%), orzeszki coli (3%), chmiel: Challenger, Fuggie, EKG, drożdże: S-04',
+    tale: `
+Założenia. Opis Smaku.
+
+Nitro Stout - czyli stout wysycony mocno azotem, który nadaje ciemnym piwom gładkości i lekkości. A żeby było ciekawiej dodaliśmy jeszcze orzeszków koli, nadających charakterystyczny delikatny posmak. W założeniu ciemne, ale lekkie piwo.
+
+Notatki
+
+Drugie piwo w serii #AleLab, w którym bierzemy na warsztat wspólnie z Wami receptury naszych piw. Oznacz swoją opinię, sugestię na Instagramie hasztagiem #AleLAB02A - a my weźmiemy ją pod uwagę przy kolejnej warce tego piwa.
+
+EleEkipa
+
+Zacieranie:
+68 stopni C - 30 min
+72 stopni C - 10 min
+72 stopni C - 10 min (słody ciemne)
+78 stopni C - 1 min
+Gotowanie: 80 minut
+    `,
+    barcode: '5907771340661',
+    container: 'puszka 500ml srebrna',
+    alcohol: '4,9 obj.',
+    extract: '14% wag.',
+    filtration: false,
+    pasteurisation: false,
+    place: 'Lębork',
+    price: 10,
+    added: '06.07.2020, 11:25',
+  },
+  {
+    brand: 'Funky Fluid',
+    name: 'Mango & Peach Party',
+    style: 'Weizen',
+    alcohol: '4.2% obj.',
+    extract: '12 stopni BLG',
+    barcode: '5907772092194',
+    contract: 'Browar Zarzecze',
+    container: 'puszka 500ml srebrna',
+    tale: {
+      pl:
+        'Przy takiej ilości mango i brzoskwiń impreza musiała skończyć się piwem, który jest jak przecier owocowy w wersji dla dorosłych. A może ona dopiero się rozpoczęła?',
+      en:
+        'With such a great amount of mango and peaches, the party just had to end with beer which is like fruit puree for adults.\n Or maybe this party has just started?',
+    },
+    ingredients:
+      'woda, słód jęczmienny, słód pszeniczny, płatki owsiane, płatki pszenne, mango, brzoskwinia, chmiele: Iunga, Marynka, drożdże',
+    filtration: false,
+    pasteurisation: true,
+    temperature: '8-10',
+    bitterness: '1/5',
+    price: 10.9,
+    added: '06.07.2020, 11:33',
+  },
+  {
+    brand: 'AleBrowar',
+    name: 'Rowing Jack',
+    style: 'India Pale Ale',
+    barcode: '5907771340609',
+    container: 'puszka 500ml srebrna',
+    alcohol: '6.2% obj.',
+    extract: '16% wag.',
+    filtration: false,
+    pasteurisation: false,
+    place: 'Lębork',
+    tale: {
+      pl:
+        'Na rufę Santa Marii! Nareszcie piwo, które nie skiśnie w drodze do Indii! Tylko ekstra porcja chmielu mogła uchronić ten drogocenny ładunek przed humorami Neptuna i wściekle palącym słońcem. Spróbuj się oprzeć marynarzu!',
+      en:
+        "At Santa Maria's stern! Finally a beer that will not go off on the way to India! Only an extra portion of hops could protect this precious cargo against the moods of Neptune and the burning sun. Try to resist, sailor!",
+    },
+    ingredients: {
+      pl:
+        'woda, słody: Pale Ale, pszeniczny, wiedeński, Carapils, zakwaszający, chmiele: Simcoe, Chinook, Citra, Cascade, Palisade, drożdże: SafAle US-05',
+      en:
+        'water, malts: Pale Ale, wheat, Vienna, Carapils, acidulated, hops: Simcoe, Chinook, Citra, Cascade, Palisade, yeast Safale US-05',
+    },
+    bitterness: '5/8',
+    sweetness: '5/8',
+    powerness: '4/8',
+    price: 12,
+    added: '06.07.2020, 11:44',
+  },
+  {
+    brand: 'Browar Stu Mostów',
+    series: 'Art',
+    name: '+36',
+    style: 'Pastry Sour Ale. Pineapple Dragon Fruit',
+    cooperation: 'Basqueland',
+    barcode: '5907614681449',
+    container: 'puszka 440ml czarna',
+    height: 15,
+    alcohol: '6.0% obj.',
+    pasteurisation: true,
+    filtration: false,
+    place: 'Wrocław',
+    contains: 'słód jęczmienny, pszeniczny, płatki owsiane, laktoza',
+    pełniasłodowosc: 85,
+    bitterness: 40,
+    price: 11.5,
+    added: '06.07.2020, 11:49',
+  },
+  {
+    brand: 'Browar Nepomucen',
+    name: 'Pijże, vol. 2',
+    extract: 'BLG 13,5 stopnia',
+    alcoholo: '4.4% obj.',
+    style: 'DDH APA',
+    cooperation: 'Weźże Krafta',
+    tale: {
+      pl:
+        'Dokładnie po roku od uwarzenia pierwszej warki naszego Krafta Roku po raz kolejny spotykamy się z naszymi przyjaciółmi z krakowskiego multitap Weźże Krafta by stworzyć drugi rozdział naszego mętnego i soczystego American Pale Ale. Tym razem na chmielach: Sabro, Enigma i Citra',
+      en:
+        'Exactly a year after brewing our first draft of our Polish Kraft of the Year, we are once again meeting our friends from the Kraków multitap Weźże Krafta to create the second chapter of our hazy and juicy American Pale Ale. This time on hops: Sabro, Enigma and Citra.',
+    },
+    place: 'Jutrosin',
+    container: 'puszka 500 ml czarna',
+    barcode: '5907709756380',
+    bitterness: '1,5/6',
+    hoprate: '18g/l',
+    ingredients: {
+      pl:
+        'woda, słód jęczmienny, płatki: owsiane, pszenne, chmiele: Sabro, Enigma, Citra, drożdże górnej fermentacji z własnej propagacji',
+      en:
+        'water, barley malt, oat flakes, wheat flakes, hops: Sabro, Enigma, Citra; top-fermenting yeast from own propagation',
+    },
+    temperature: '10-12',
+    filtration: false,
+    pasteurisation: true,
+    price: 11.3,
+    added: '06.07.2020, 11:59',
+    height: 17,
+  },
 ];
