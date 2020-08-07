864d41c5ff0421e1356d1e53bd30938ae3989238
Tomek Rozalski <alium@daucusetanser.pl>
2020-08-07 15:22:53 +0200

    [Beverage] Refactor Alcohol Scope values to be simple strings like m500, pm500, pm1000

--- src/utils/schemas/beverage/editorialSchema.ts
+++ src/utils/schemas/beverage/editorialSchema.ts
@@ -5,103 +5,130 @@ import { langValue } from '../common';
 import agedSchema from './agedSchema';
 import priceSchema from './priceSchema';
 
-const generalSchema = new mongoose.Schema({
-	cooperation: {
-		type: [{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Institution',
-		}],
-		default: undefined,
-	},
-	contract: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Institution',
-	},
-	place: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Place',
-	},
-}, { _id: false });
+const generalSchema = new mongoose.Schema(
+  {
+    cooperation: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Institution',
+        },
+      ],
+      default: undefined,
+    },
+    contract: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Institution',
+    },
+    place: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Place',
+    },
+  },
+  { _id: false },
+);
 
-const brewingSchema = new mongoose.Schema({
-	fermentation: {
-		type: [{
-			type: String,
-			enum: ['top', 'bottom', 'spontaneous'],
-		}],
-		default: undefined,
-	},
-	alcohol: {
-		scope: {
-			type: String,
-			enum: ['<0.5%', '±0.5%', '±1.0%'],
-		},
-	},
-	filtration: Boolean,
-	pasteurization: Boolean,
-	aged: {
-		type: [agedSchema],
-		default: undefined,
-	},
-	style: {
-		type: [langValue],
-		default: undefined,
-	},
-	isDryHopped: Boolean,
-	dryHopped: {
-		hops: {
-			type: [{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'Ingredients',
-			}],
-			default: undefined,
-		},
-	},
-}, { _id: false });
+const brewingSchema = new mongoose.Schema(
+  {
+    fermentation: {
+      type: [
+        {
+          type: String,
+          enum: ['top', 'bottom', 'spontaneous'],
+        },
+      ],
+      default: undefined,
+    },
+    alcohol: {
+      scope: {
+        type: String,
+        enum: ['m500', 'pm500', 'pm1000'],
+      },
+    },
+    filtration: Boolean,
+    pasteurization: Boolean,
+    aged: {
+      type: [agedSchema],
+      default: undefined,
+    },
+    style: {
+      type: [langValue],
+      default: undefined,
+    },
+    isDryHopped: Boolean,
+    dryHopped: {
+      hops: {
+        type: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Ingredients',
+          },
+        ],
+        default: undefined,
+      },
+    },
+  },
+  { _id: false },
+);
 
-const impressionsSchema = new mongoose.Schema({
-	color: String,
-	clarity: {
-		type: String,
-		enum: ['crystalline', 'clear', 'opalescent', 'misty', 'hazy', 'muddy'],
-	},
-}, { _id: false });
+const impressionsSchema = new mongoose.Schema(
+  {
+    color: String,
+    clarity: {
+      type: String,
+      enum: ['crystalline', 'clear', 'opalescent', 'misty', 'hazy', 'muddy'],
+    },
+  },
+  { _id: false },
+);
 
-const coverImageSchema = new mongoose.Schema({
-	height: Int32,
-	width: Int32,
-}, { _id: false });
+const coverImageSchema = new mongoose.Schema(
+  {
+    height: Int32,
+    width: Int32,
+  },
+  { _id: false },
+);
 
-const outlinesSchema = new mongoose.Schema({
-	cover: String,
-	gallery: String,
-}, { _id: false });
+const outlinesSchema = new mongoose.Schema(
+  {
+    cover: String,
+    gallery: String,
+  },
+  { _id: false },
+);
 
-const photosSchema = new mongoose.Schema({
-	cap: {
-		type: Boolean,
-		validate: {
-			validator(v) {
-				return v;
-			},
-			message: props => `${props.value} need to be true or be undefined`,
-		},
-	},
-	cover: coverImageSchema,
-	gallery: Int32,
-	outlines: outlinesSchema,
-}, { _id: false });
+const photosSchema = new mongoose.Schema(
+  {
+    cap: {
+      type: Boolean,
+      validate: {
+        validator(v) {
+          return v;
+        },
+        message: props => `${props.value} need to be true or be undefined`,
+      },
+    },
+    cover: coverImageSchema,
+    gallery: Int32,
+    outlines: outlinesSchema,
+  },
+  { _id: false },
+);
 
-const editorialSchema = new mongoose.Schema({
-	general: generalSchema,
-	brewing: brewingSchema,
-	impressions: impressionsSchema,
-	price: {
-		type: [priceSchema],
-		default: undefined,
-	},
-	photos: photosSchema,
-	notes: String,
-}, { _id: false });
+const editorialSchema = new mongoose.Schema(
+  {
+    general: generalSchema,
+    brewing: brewingSchema,
+    impressions: impressionsSchema,
+    price: {
+      type: [priceSchema],
+      default: undefined,
+    },
+    photos: photosSchema,
+    notes: String,
+  },
+  { _id: false },
+);
 
 export default editorialSchema;
